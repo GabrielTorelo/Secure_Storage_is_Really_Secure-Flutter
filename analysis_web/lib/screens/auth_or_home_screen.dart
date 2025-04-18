c7594e54a0c0b7ad50d30b71ffa47dd67d07f5e8
Gabriel Torelo <gabrieltorelo@gmail.com>
2025-04-18 20:44:56 -0300

    feat(auth_or_home_screen): creates screen to handle authentication

---
+++ analysis_web/lib/screens/auth_or_home_screen.dart
@@ -0,0 +1,34 @@
+import 'package:analysis_web/components/background_gradient.dart';
+import 'package:analysis_web/notifier/auth_notifier.dart';
+import 'package:analysis_web/screens/auth_screen.dart';
+import 'package:analysis_web/screens/error_screen.dart';
+import 'package:analysis_web/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class AuthOrHomeScreen extends StatelessWidget {
+  const AuthOrHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final AuthNotifier authNotifier = Provider.of<AuthNotifier>(context);
+
+    return FutureBuilder(
+      future: authNotifier.tryAutoLogin(),
+      builder: (ctx, snapshot) {
+        return switch (snapshot.connectionState) {
+          ConnectionState.waiting => Scaffold(
+              body: BackgroundGradient(
+                content: const Center(
+                  child: CircularProgressIndicator(),
+                ),
+              ),
+            ),
+          ConnectionState.done =>
+            authNotifier.isAuth ? const HomeScreen() : const AuthScreen(),
+          _ => const ErrorScreen(),
+        };
+      },
+    );
+  }
+}
